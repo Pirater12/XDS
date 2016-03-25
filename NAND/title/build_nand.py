c896a8699377da9a1bdc080f09a3f7307123f428
Yifan Lu <me@yifanlu.com>
2016-03-24 20:55:03 -0500

    Added NAND dump generation with decrypted CIAs Added basic build instructions to readme

---
+++ NAND/title/build_nand.py
@@ -0,0 +1,25 @@
+#!/bin/python
+import glob
+import os
+import sys
+
+def main():
+  if len(sys.argv) < 2:
+    print 'invalid directory specified'
+    return
+
+  for i in os.listdir(sys.argv[1]):
+    tid_high = i[0:8]
+    tid_low = i[8:16]
+    outdir = os.path.join('.', tid_high, tid_low, 'content')
+    if not os.path.exists(outdir):
+      os.makedirs(outdir)
+    cia = os.path.join(sys.argv[1], i)
+    os.system("./ctrtool --tmd=" + outdir + "/00000000.tmd --contents=" + outdir + "/contents '" + cia + "'")
+    items = glob.glob(outdir + '/contents.*')
+    for item in items:
+      cid = item[-8:]
+      os.rename(item, os.path.join(outdir,cid + '.app'))
+
+if __name__ == "__main__":
+  main()
--- README.md
+++ README.md
@@ -5,3 +5,12 @@ plutooo
 Normmatt
 Bond697
 who helped me create XDS
+
+To build XDS, first build external/gl3w
+On Windows, use windows/xds.sln
+On OSX, install libglfw3 (Makefile assumes its installed with MacPorts), then 
+run `make`
+
+To generate a NAND dump of the firmware, decrypt all CIAs for the firmware and 
+run `python NAND/title/build_nand.py dir/to/cias` and the decrypted firmware 
+will be placed into the correct directories. You also need ctrtool for this.
